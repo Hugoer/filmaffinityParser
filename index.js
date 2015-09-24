90f233850313fe2206a31fa15b01366e0c41555e
Hugoer <kuky85@gmail.com>
2015-09-24 18:13:05 +0200

    fix some bugs - overflow

--- index.js
+++ index.js
@@ -58,14 +58,8 @@ var downloadThumbnail = function(uri, name ,callback){
         }
 
     }
-
 };
 
-/*// Queue just one URL, with default callback
-c.queue('http://joshfire.com');
-
-// Queue a list of URLs
-c.queue(['http://jamendo.com/','http://tedxparis.com']);*/
 
 function Film( filmDirector, filmTitle, filmActors, genres, topic, synopsis, originalTitle, year, country, rating, ratingCount ){
 
@@ -86,45 +80,6 @@ function Film( filmDirector, filmTitle, filmActors, genres, topic, synopsis, ori
 var filmAffinitySearch = function( title ){
     return 'http://www.filmaffinity.com/es/search.php?stype=title&stext=' + encodeURIComponent( title );
 };
-
-// var getLengthByIndex = function( character ){
-//     var urlDummie = config.urlMain + character + '_1.html'
-//     c.queue({
-//       uri: urlDummie,
-//         jQuery: true,
-// userAgent: config.userAgent,
-//         forceUTF8: true,
-//         callback: function (error, result, $) {
-//             return 
-//         }
-//     });    
-// };
-
-// insertIdFilm = function( idFilm ){
-//     var mongoose = require('mongoose');
-//     mongoose.connect('mongodb://localhost:27017/films');
-
-//     var db = mongoose.connection;
-
-//     // db.on('error', function (err) {
-// log('connection error', err);
-//     // });
-
-//     // db.on('open', function () {
-//     //     // log('Opennnn');
-//     // });
-
-//     var myFilm = require('./models/film');
-//     var filmInsert = new myFilm(idFilm);
-
-//     filmInsert.save(function( err, data ){
-        
-// log(err);
-//         // log('Close');
-//         db.close();  
-//     })
-// };
-
 
 getAllUrl = function( urlFilmList ){
     log('getAllUrl - urlFilmList.length : ' + urlFilmList.length );
@@ -249,9 +204,145 @@ getAllUrl = function( urlFilmList ){
     c.queue(urlFilmList);    
 }
 
-getAllFilmsByCharacter = function( urlHtmlIndex ){    
-    log('urlHtmlIndex on getAllFilmsByCharacter : ' + urlHtmlIndex.length );
-    var movieId = [];
+insertAllIdFilm = function( idFilmList ){
+    var filmList = [];
+    var c = new Crawler({
+        maxConnections : config.maxConnections,
+        jQuery: true,
+        userAgent: config.userAgent,
+        forceUTF8: true,
+        callback: function (error, result, $) {
+            var $actors,
+                $genres,
+                $topics,
+                actorsList = [],
+                genresList = [],
+                topicsList = [],
+                title,
+                synopsis,
+                originalTitle,
+                year,
+                country,
+                rating,
+                ratingCount,
+                thumbnailName;
+
+            if (error) {
+                log(error);
+            }else{
+                // log('InGetDataFromUrl');
+                try{
+                    $actors = $($('.movie-info')[0]).find('a[href*="/es/search.php?stype=cast"]');
+                    $genres = $($('.movie-info')[0]).find('a[href*="/es/moviegenre.php?genre"]');
+                    $topics = $($('.movie-info')[0]).find('a[href*="/es/movietopic.php?topic"]');
+
+                    title = $('#main-title span').text();
+                    synopsis = $($($('.movie-info')[0]).find('dd').last()[0]).text();
+                    originalTitle = $($('.movie-info dd')[0]).text().trim();
+                    year = $($($('.movie-info')[0]).find('dd[itemprop="datePublished"]')[0]).text();
+                    country = $('#country-img').parent().text().trim();
+                    rating = $('#movie-rat-avg').text().trim();
+                    ratingCount = $('#movie-count-rat span').text();
+                    thumbnailName = $('#movie-main-image-container img').attr('src');
+
+                    for (var i = 0; i < $actors.length; i++) {
+                        actorsList.push( $($actors[i]).text().trim() );
+                    };
+
+                    for (var i = 0; i < $genres.length; i++) {
+                        genresList.push( $($genres[i]).text().trim() );
+                    };
+
+                    for (var i = 0; i < $topics.length; i++) {
+                        topicsList.push( $($topics[i]).text().trim() );
+                    };                
+
+                    log('Recogiendo datos de: ' + title);
+
+                    var film = new Film( 
+                                    $('.directors span a span').text() ,
+                                    title,
+                                    actorsList,
+                                    genresList,
+                                    topicsList,
+                                    synopsis,
+                                    originalTitle,
+                                    year,
+                                    country,
+                                    rating,
+                                    ratingCount
+                                );
+
+                    // log( film.toString() );
+
+                    thumbnailName = thumbnailName.substring(thumbnailName.lastIndexOf('/')+1,thumbnailName.length);
+
+                    if ( config.downloadImages ){
+                        giSearch.search( title + ' ' + year + ' filmaffinity', function (err, images) {
+                            
+                            if ( images[0] ){
+                                // downloadThumbnail( images[0].unescapedUrl, thumbnailName );
+                                log ('descarga hecha: ' + images[0].unescapedUrl ) ;
+                            }
+                        });
+                    }
+
+                    // jsonfile.writeFileSync( config.pathFile + originalTitle.split(' ').join('_') + '.JSON' , film);
+                    //film
+                    filmList.push(film);
+
+                }catch(err){
+                    log ('Error: ' + err);
+                }
+
+            }
+        },
+        onDrain: function(){
+            log('2 - onDrain - insertAllIdFilm');
+            var mongoose = require('mongoose');
+            mongoose.connect('mongodb://localhost:27017/films');
+
+            var db = mongoose.connection;
+
+            var myFilmDet = require('./models/filmDet');
+            //movieId inserta tooooodos los ids pero en un campo y separados por comas.
+            var filmInsert;
+            var res = [];
+
+            for (var i = 0; i < filmList.length; i++) {
+                log('2 - Insertando datos de: ' + filmList[i].title );
+                filmInsert = new myFilmDet( filmList[i] );
+                filmInsert.save(function (err) {
+                    res.push(err);
+                });                
+            };
+
+            db.close();
+
+            log('2 - idFilmList.length: ' + idFilmList.length);
+
+            idFilmList.splice(0,config.maxArrayElements);
+
+            log('2 - idFilmList.length: ' + idFilmList.length);
+
+            if ( idFilmList.length > 0 ){
+                insertAllIdFilm(idFilmList);
+            }else{
+                log('FINISHED!');
+            }
+
+            if ( res.length > 0  ) log(res);
+
+        }
+    });
+    
+    //Número total de películas
+    log('2 - idFilmList.length: ' + idFilmList.length);
+    c.queue(idFilmList.slice(0,config.maxArrayElements));
+}
+
+getAllFilmsByCharacter = function( urlHtmlIndex ){
+    
     var c = new Crawler({
         maxConnections : config.maxConnections,
         jQuery: true,
@@ -261,10 +352,8 @@ getAllFilmsByCharacter = function( urlHtmlIndex ){
             var movieUrl = '';
             // log(result);
             $('.fa-shadow').each(function(index, a) {
-                // movieUrl = config.initialUrl + $($(a).find('.mc-title a')[0]).attr('href');
-                // getDataFromUrl( movieUrl );
                 movieUrl = $(a).find('.movie-card').data('movie-id');
-                log(config.urlFilm + movieUrl + '.html');
+                log('1 - callback - ' + config.urlFilm + movieUrl + '.html');
                 movieId.push( config.urlFilm + movieUrl + '.html');       
 
             });
@@ -272,24 +361,26 @@ getAllFilmsByCharacter = function( urlHtmlIndex ){
         },
         onDrain: function(){
 
-            getAllUrl ( movieId.slice(1,config.maxArrayElements) );
+            log('1 - onDrain - urlHtmlIndex.length: ' + urlHtmlIndex.length + ' páginas.');
+            
+            urlHtmlIndex.splice(0,config.maxArrayElements);
+            
+            log('1 - onDrain - urlHtmlIndex.length: ' + urlHtmlIndex.length + ' páginas.');
+
+            if ( urlHtmlIndex.length > 0 ){
+                getAllFilmsByCharacter(urlHtmlIndex);
+            }else{
+                log('1 - Total películas: ' + movieId.length);
+                insertAllIdFilm ( movieId );
+            }
         }
     });
+    
+    //http://www.filmaffinity.com/es/allfilms_A_372.html
+    log('1 - urlHtmlIndex.length : ' + urlHtmlIndex.length );
+    c.queue( urlHtmlIndex.slice(0,config.maxArrayElements) );
 
-    //== Hacer un bucle para no meter tantísimas llamadas ( es un objeto con 114.000 urls. )
-    //== Poner algo que se sepa el progreso lo más preciso posible.
-    //== Revisar en 0-9 por qué faltan películas en la base de datos.
-    //== Control de errores en "Insertando datos de: "
-    var maxArrayElements = config.maxArrayElements,
-        arraySliced = [];
-
-    arraySpliced = urlHtmlIndex.slice(1,maxArrayElements);
-    log('maxArrayElements ' + maxArrayElements );
-    log('urlHtmlIndex ' + arraySpliced.length);
-    c.queue(arraySpliced);
 };
-
-
 
 var getAllFilmsUrls = function(){
 
@@ -333,10 +424,8 @@ var getAllFilmsUrls = function(){
                 if (lastIndex) {
                     
                     for (var j = 0; j < lastIndex; j++) {
-                        var urlHtmlIndex = config.urlMain + chr + '_'+ j + '.html';
-                        // log(indexSeparator  + ' - ' + chr + ' - ' + j + ' - ' + lastIndex );
-                        // getAllFilmsByCharacter( urlHtmlIndex );
-                        log(urlHtmlIndex)
+                        var urlHtmlIndex = config.urlMain + chr + '_'+ (j+1) + '.html';
+                        log('0 - ' + urlHtmlIndex)
                         filmId.push(urlHtmlIndex);
                     };
                 }
@@ -348,17 +437,15 @@ var getAllFilmsUrls = function(){
             if (filmId.length > 0){
                 getAllFilmsByCharacter( filmId );
             }else{
-                log('No Results on getAllFilmsUrls');
+                log('0 - ' + 'No Results on getAllFilmsUrls');
             }
             
         }
     });
 
-    log(filmsUrls.length);
+    log('0 - filmsUrls.length: ' + filmsUrls.length);
 
     c.queue(filmsUrls);
-
-
 };
 
 var getDataFromUrl = function( url ){
@@ -478,34 +565,5 @@ var getDataFromUrl = function( url ){
     });    
 };
 
+var movieId = [];
 getAllFilmsUrls();
-
-// getDataFromUrl('http://www.filmaffinity.com//es/film186215.html');
-// getDataFromUrl('http://www.filmaffinity.com/es/film832057.html');
-
-//Realizar búsqueda por película:
-// c.queue({
-//   uri: filmAffinitySearch('Harry Potter'),
-//     jQuery: true,
-//     userAgent: config.userAgent,
-//     forceUTF8: true,
-//     callback: function (error, result, $) {
-
-//         var url;
-
-//         if (error) {
-//             log(error);
-//         }else{
-//             log( $('#mt-content-cell #main-title').text() );
-//             $('.fa-shadow').each(function(index, a) {
-//                 url = config.initialUrl + $($(a).find('.mc-title a')[0]).attr('href');
-//                 getDataFromUrl( url );
-//             });
-//         }
-
-//     }  
-// });
-/*
-Crear fichero log ( Hora Inicio + cambio de 'character' + Hora Fin )
-Diferenciar entre series y películas.
-*/
